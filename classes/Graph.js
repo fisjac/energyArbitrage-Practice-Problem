115ce05a179161d84556ca18761c0c851a13da60
fisjac <jfisher9882@gmail.com>
2022-07-17 12:53:13 -0500

    added Graph specs

--- classes/Graph.js
+++ classes/Graph.js
@@ -35,49 +35,49 @@ class Graph {
     ---------------------------`);
 
     // traverse to every terminal node in the graph
-      let stack = [this.root];
-      while (stack.length) {
-        let currentNode = stack.pop();
-        let [chargeNode, waitNode, dischargeNode] = currentNode.getNext();
-        if (!(chargeNode || waitNode || dischargeNode)) {
+    let stack = [this.root];
+    while (stack.length) {
+      let currentNode = stack.pop();
+      let [chargeNode, waitNode, dischargeNode] = currentNode.getNext();
+      if (!(chargeNode || waitNode || dischargeNode)) {
 
-          // if there's space in the battery add to charge path
-          if (currentNode.energy < this.duration) {
-            const newNode = new GraphNode(price);
-            currentNode.charge = newNode;
-            // Set newNodes energy to currentNode's energy + 1
-            currentNode.charge.energy = currentNode.energy + 1;
-            currentNode.charge.profit = currentNode.profit - price;
-            // console.log(`added to charge path. Energy is now at ${currentNode.charge.energy}`);
-          };
-
-          // if there's energy in the battery add to discharge path
-          if (currentNode.energy > 0) {
-            const newNode = new GraphNode(price);
-            currentNode.discharge = newNode;
-            // Set newNodes energy to currentNode's energy + 1
-            currentNode.discharge.energy = currentNode.energy - 1;
-            currentNode.discharge.profit = currentNode.profit + price;
-            // console.log(`added to discharge path. Energy is now at ${currentNode.discharge.energy}`);
-          };
-
-          // add new node to wait path
+        // if there's space in the battery add to charge path
+        if (currentNode.energy < this.duration) {
           const newNode = new GraphNode(price);
-          currentNode.wait = newNode;
-          // maintain energy level
-          currentNode.wait.energy = currentNode.energy;
-          currentNode.wait.profit = currentNode.profit;
-          // console.log(`added to wait path. Energy is now at ${currentNode.wait.energy}`);
-        } else {
-          [chargeNode, waitNode, dischargeNode].forEach(nextNode => {
-            if (nextNode) stack.push(nextNode);
-          })
+          currentNode.charge = newNode;
+          // Set newNodes energy to currentNode's energy + 1
+          currentNode.charge.energy = currentNode.energy + 1;
+          currentNode.charge.profit = currentNode.profit - price;
+          // console.log(`added to charge path. Energy is now at ${currentNode.charge.energy}`);
+        };
 
-        }
+        // if there's energy in the battery add to discharge path
+        if (currentNode.energy > 0) {
+          const newNode = new GraphNode(price);
+          currentNode.discharge = newNode;
+          // Set newNodes energy to currentNode's energy + 1
+          currentNode.discharge.energy = currentNode.energy - 1;
+          currentNode.discharge.profit = currentNode.profit + price;
+          // console.log(`added to discharge path. Energy is now at ${currentNode.discharge.energy}`);
+        };
+
+        // add new node to wait path
+        const newNode = new GraphNode(price);
+        currentNode.wait = newNode;
+        // maintain energy level
+        currentNode.wait.energy = currentNode.energy;
+        currentNode.wait.profit = currentNode.profit;
+        // console.log(`added to wait path. Energy is now at ${currentNode.wait.energy}`);
+      } else {
+        [chargeNode, waitNode, dischargeNode].forEach(nextNode => {
+          if (nextNode) stack.push(nextNode);
+        })
+
       }
     }
   }
 }
 
 
-module.exports = {GraphNode, Graph};
+
+module.exports = { GraphNode, Graph };
--- test/class-specs.js
+++ test/class-specs.js
@@ -15,16 +15,39 @@ describe('GraphNode', () => {
 });
 
 
-// this.price = price;
-// this.energy = 0;
-// this.profit = 0;
+describe('Graph', () => {
 
-// // branches
-// this.charge = null;
-// this.wait = null;
-// this.discharge = null;
-// }
+  context('can add nodes properly', () => {
+    it('has an insert method', () => {
+      expect(Graph).to.respondTo('insert');
+    })
 
+    let graph1 = new Graph(1)
+    let graph2 = new Graph(2)
+    graph1.insert(1);
+    graph1.insert(2);
+    graph1.insert(3);
 
-// getNext() {
-// return [this.charge, this.wait, this.discharge];
+    graph2.insert(1);
+    graph2.insert(2);
+    graph2.insert(3);
+
+    it('updates the energy state for the next node', ()=> {
+      expect(graph1.root.charge.energy).to.equal(1);
+      expect(graph1.root.wait.energy).to.equal(0);
+      expect(graph1.root.charge.discharge.energy).to.equal(0);
+    })
+
+    it('adds nodes on the correct paths', () => {
+      expect(graph1.root.charge).to.not.be.null;
+      expect(graph1.root.discharge).to.be.null;
+      expect(graph1.root.charge.discharge).to.not.be.null;
+    })
+
+    it('uses duration to inform the available paths', ()=> {
+      expect(graph1.root.charge.charge).to.be.null;
+      expect(graph2.root.charge.charge).to.not.be.null;
+      expect(graph2.root.charge.charge.charge).to.be.null;
+    })
+  })
+})
